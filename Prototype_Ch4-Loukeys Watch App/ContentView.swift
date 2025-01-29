1d78f652532dab624b52bae4967c7764ed1d17dd
Daniele Fontana <danyfo00@gmail.com>
2025-01-29 09:45:02 +0100

    protUpdate1

--- Prototype_Ch4-Loukeys Watch App/ContentView.swift
+++ Prototype_Ch4-Loukeys Watch App/ContentView.swift
@@ -6,18 +6,65 @@
 //
 
 import SwiftUI
+import UserNotifications
+import AVFoundation
 
 struct ContentView: View {
+    @State private var taskCompleted = false
+    @State private var player: AVAudioPlayer?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Prendi la pillola")
+                .font(.headline)
+                .padding()
+            
+            if taskCompleted {
+                Text("Completato! ✅")
+                    .foregroundColor(.green)
+                    .font(.subheadline)
+            } else {
+                Button(action: {
+                    taskCompleted = true
+                    playSound()
+                    scheduleNotification()
+                }) {
+                    Text("Segna come fatto")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
         }
-        .padding()
+    }
+    
+    
+    
+    func playSound() {
+        if let soundURL = Bundle.main.url(forResource: "success", withExtension: "wav") {
+            do {
+                player = try AVAudioPlayer(contentsOf: soundURL)
+                player?.play()
+            } catch {
+                print("Errore nella riproduzione del suono: \"(error) /")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Promemoria"
+        content.body = "Ricorda di prendere la pillola!"
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
     }
 }
+
 
 #Preview {
     ContentView()
