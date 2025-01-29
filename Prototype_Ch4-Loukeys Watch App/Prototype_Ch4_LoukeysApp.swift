2814a465e45fa7e0013d6f740a3599ea67d305ad
Daniele Fontana <danyfo00@gmail.com>
2025-01-29 08:54:23 +0100

    first commit

---
+++ Prototype_Ch4-Loukeys Watch App/Prototype_Ch4_LoukeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype_Ch4_LoukeysApp.swift
+//  Prototype_Ch4-Loukeys Watch App
+//
+//  Created by Daniele Fontana on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Prototype_Ch4_Loukeys_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
